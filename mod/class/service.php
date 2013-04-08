b7b6e6816f0f95d2952e458def39b778d697e565
Alexandr Golikov <golikov.org@gmail.com>
2013-04-08 13:08:51 +0400

    mod_service::get исправил тупняк

--- mod/class/service.php
+++ mod/class/service.php
@@ -5,29 +5,30 @@
  **/
 class mod_service extends mod_controller {
 
-	public function defaultService() {
-	    return false;
-	}
-
-    public static function serviceFactory() {
-        return new get_called_class();
+    public function defaultService() {
+        return false;
     }
 
-	public static function get($name) {
-	
-	    $class = mod_conf::general("services",$name,"class");
-	    
-	    if(!$class) {
-		    $services = mod::classmap("services");
-		    $class = $services[$name];
-	    }
-	    
-	    if(!$class) {
-	        throw new Exception("Service [$name] not found");
-	    }
-	    
-	    return $class::serviceFactory();
-	
-	}
+    public static function serviceFactory() {
+        $class = get_called_class();
+        return new $class;
+    }
+
+    public static function get($name) {
+
+        $class = mod_conf::general("services",$name,"class");
+
+        if(!$class) {
+            $services = mod::classmap("services");
+            $class = $services[$name];
+        }
+
+        if(!$class) {
+            throw new Exception("Service [$name] not found");
+        }
+
+        return $class::serviceFactory();
+
+    }
 
 }
