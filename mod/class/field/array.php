bce59bc6dee8a29fc8310be9b54ad2b8f933b699
Alexander <goilikov.org@gmail.com>
2013-06-23 03:56:33 +0400

    Сделал красивое табличное представление mod_field_array

--- mod/class/field/array.php
+++ mod/class/field/array.php
@@ -2,42 +2,50 @@
 
 class mod_field_array extends mod_field {
 
-	public function typeID() {
-		return "puhj-w9sn-c10t-85bt-8e67";
-	}
-	
-	public function typeName() {
-		return "Массив";
-	}
+    public function typeID() {
+        return "puhj-w9sn-c10t-85bt-8e67";
+    }
 
-	public function mysqlType() {
-		return "blob";
-	}
+    public function typeName() {
+        return "Массив";
+    }
 
-	public function mysqlIndexFields() {
-		return $this->name()."(1)";
-	}
+    public function mysqlType() {
+        return "blob";
+    }
 
-	public function editorInx() {
-		return array(
-		    "type" => "inx.mod.reflex.fields.arr",
-		    "value" => $this->value(),
-		);
-	}
+    public function mysqlIndexFields() {
+        return $this->name()."(1)";
+    }
 
-	public function pvalue() {
-		$ret = json_decode($this->value(),1);
-		if(!is_array($ret)) {
-			$ret = array();
-		}
-		return $ret;
-	}
+    public function editorInx() {
+        return array(
+            "type" => "inx.mod.reflex.fields.arr",
+            "value" => $this->value(),
+        );
+    }
 
-	public function prepareValue($val) {
-		if(is_array($val)) {
-			$val = json_encode($val);
-		}
-		return $val;
-	}
+    public function pvalue() {
+        $ret = json_decode($this->value(),1);
+        if(!is_array($ret)) {
+            $ret = array();
+        }
+        return $ret;
+    }
+
+    public function prepareValue($val) {
+        if(is_array($val)) {
+            $val = json_encode($val);
+        }
+        return $val;
+    }
+
+    public function tableRender() {
+        $ret = array();
+        foreach($this->pvalue() as $key=>$val) {
+            $ret[] = $key.":".$val;
+        }
+        return implode(", ",$ret);
+    }
 
 }
