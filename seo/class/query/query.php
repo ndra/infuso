ac20fce1af06f4eca84e0a13aed6e502109336c9
Alexander <goilikov.org@gmail.com>
2013-07-12 11:30:50 +0400

    Перевел считалку позиций на новый reflex_task

--- seo/class/query/query.php
+++ seo/class/query/query.php
@@ -1,74 +1,96 @@
 <?
 
-	class seo_query extends reflex implements mod_handler {
+class seo_query extends reflex implements mod_handler {
 
-	public static function all() { return reflex::get(get_class())->joinByField("domain")->neq("seo_domain.id",0)->asc("priority"); }
-	public static function get($id) { return reflex::get(get_class(),$id); }
+    public static function all() {
+        return reflex::get(get_class())->joinByField("domain")->neq("seo_domain.id",0)->asc("priority");
+    }
 
-	public function reflex_title() { return $this->data("query"); }
-	public function _domain() { return seo_domain::get($this->data("domain")); }
-	public function reflex_parent() { return $this->_domain(); }
+    public static function get($id) {
+        return reflex::get(get_class(),$id);
+    }
 
-	public function reflex_cleanup() {
-	    if(!$this->_domain()->exists()) return true;
-	}
+    public function reflex_title() {
+        return $this->data("query");
+    }
 
-	public function positions() {
-	    return seo_query_position::all()->eq("queryID",$this->id());
-	}
+    public function _domain() {
+        return seo_domain::get($this->data("domain"));
+    }
 
-	public function reflex_children() {
-	    return array(
-	        $this->positions()->title("Позиции"),
-	    );
-	}
+    public function reflex_parent() { return $this->_domain(); }
 
-	public function reflex_repair() { $this->setUpdateTime(); }
+    public function reflex_cleanup() {
+        if(!$this->_domain()->exists()) return true;
+    }
 
-	// ----------------------------------------------------------------
+    public function positions() {
+        return seo_query_position::all()->eq("queryID",$this->id());
+    }
 
-	public function setUpdateTime() {
-	    $d = 0;
-	    foreach($this->_domain()->engines() as $engine) {
-	        $date = $this->positions()->eq("engineID",$engine->id())->max("date");
-	        $date = util::date($date)->stamp();
-	        if($d==0) {
-	            $d = $date;
-	        } else {
-	            if($date<$d) $d = $date;
-	        }
-	    }
-	    $this->data("update",util::date($date));
-	}
+    public function reflex_children() {
+        return array(
+            $this->positions()->title("Позиции"),
+        );
+    }
 
-	public static function refreshLast() {
-	    $now = util::now()->notime()."";
-	    $query = self::all()->where("date(`update`)<>'$now' or `update` is null ")->one();
-	    if($query->exists())
-	        $query->refresh();
-	}
+    public function reflex_repair() {
+        $this->setUpdateTime();
+    }
 
-	public function on_mod_cron() {
-		self::refreshLast();
-	}
+    public function setUpdateTime() {
+        $d = 0;
+        foreach($this->_domain()->engines() as $engine) {
+            $date = $this->positions()->eq("engineID",$engine->id())->max("date");
+            $date = util::date($date)->stamp();
+            if($d==0) {
+                $d = $date;
+            } else {
+                if($date<$d) $d = $date;
+            }
+        }
+        $this->data("update",util::date($date));
+    }
 
-	public function refresh() {
-	    foreach($this->_domain()->engines() as $engine) {
-	        $position = seo_query_position::all()->eq("date",util::now()->notime())->eq("engineID",$engine->id())->eq("queryID",$this->id())->one();
-	        if(!$position->exists()) {
-	            $position = reflex::create("seo_query_position",array(
-	                "engineID" => $engine->id(),
-	                "queryID" => $this->id(),
-	            ));
-	            $callback = $engine->data("callback");
-	            $callback = explode("::",$callback);
-	            $result = call_user_func($callback,$this,$engine->data("callbackParam"));
-	            $position->data("position",$result["position"]);
-	            $position->data("url",$result["url"]);
-	            return;
-	        }
-	    }
-	    $this->setUpdateTime();
-	}
+    public static function refreshLast() {
+
+        $now = util::now()->notime()."";
+        $queries = self::all()
+            ->joinByField("domain")
+            ->where("date(`update`)<>'$now' or `update` is null ");
+
+        $query = $queries->one();
+
+        if($query->exists()) {
+            $query->refresh();
+        }
+    }
+
+    public function on_mod_init() {
+        reflex_task::add(array(
+            "class" => get_class(),
+            "method" => "refreshLast"
+        ));
+    }
+
+    public function refresh() {
+
+        foreach($this->_domain()->engines() as $engine) {
+            $position = seo_query_position::all()->eq("date",util::now()->notime())->eq("engineID",$engine->id())->eq("queryID",$this->id())->one();
+            if(!$position->exists()) {
+                $position = reflex::create("seo_query_position",array(
+                    "engineID" => $engine->id(),
+                    "queryID" => $this->id(),
+                ));
+                $callback = $engine->data("callback");
+                $callback = explode("::",$callback);
+                $result = call_user_func($callback,$this,$engine->data("callbackParam"));
+                $position->data("position",$result["position"]);
+                $position->data("url",$result["url"]);
+                return;
+            }
+        }
+        $this->setUpdateTime();
+    }
 
 }
