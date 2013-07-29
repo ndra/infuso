0c850685b07ab573c73ebc15f599c06c04d18a8d
alkogolikov <golikov.org@gmail.com>
2013-07-29 21:10:38 +0400

    Восстановил случайно удаленный файл

---
+++ reflex/class/admin/tables.php
@@ -0,0 +1,16 @@
+<?
+
+class reflex_admin_tables extends mod_controller {
+
+    public function indexTest() {
+        return mod_superadmin::check();
+    }
+
+    public function index($p) {
+        tmp::exec("/reflex/admin/tables",array(
+            "class" => $p["class"],
+            "search" => $p["search"],
+        ));
+    }
+
+}
