45b0fe1b2d1fe85a4db7acf1a2649f8652a5b7d8
alkogolikov <golikov.org@gmail.com>
2013-11-29 11:49:24 +0400

    Навожу порядок

--- reflex/class/route/route.php
+++ reflex/class/route/route.php
@@ -131,10 +131,12 @@ class reflex_route extends mod_route implements mod_handler {
 	    if($action->action()=="item") {
 			$id = $action->param("id");
 			$obj = reflex::get($action->className(),$id);
-			if(!$obj->published()) {
-				mod_cmd::error(404);
+			if($obj->published()) {
+			    $action->ar(get_class($obj)."/".$obj->id());
+			} else {
+			    mod_cmd::error(404);
 			}
-			$action->ar(get_class($obj)."/".$obj->id());
+			
 		}
 
 		
