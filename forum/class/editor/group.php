d5e46ab004c4f4d1001e507584aba07578edf8c1
Kopleman <kopleman@spark-mail.ru>
2014-06-18 11:30:37 +0400

    отключил дефолтный вывод формов в каталог

--- forum/class/editor/group.php
+++ forum/class/editor/group.php
@@ -11,13 +11,13 @@ class forum_editor_group extends reflex_editor {
      *
      * @return array
      **/
-     public function root() {
-		return array(
-			forum_group::root()
-				->param("sort",true)
-				->title("Группы"),
-		);
-     }
+     /*public function root() {
+        return array(
+            forum_group::root()
+                ->param("sort",true)
+                ->title("Группы"),
+        );
+     }*/
         
     public function beforeEdit() {
         return user::active()->checkAccess("forum:editGroup");
